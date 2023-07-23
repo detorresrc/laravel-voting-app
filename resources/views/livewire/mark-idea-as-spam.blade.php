8ad318130cff06d15c2242e3f9119e361a801d7a
Rommel de Torres <detorresrc@gmail.com>
2023-07-23 22:52:27 +0800

    Refactor modal confirm

--- resources/views/livewire/mark-idea-as-spam.blade.php
+++ resources/views/livewire/mark-idea-as-spam.blade.php
@@ -1,47 +1,7 @@
-<div
-    x-cloak
-    x-init="
-        window.livewire.on('ideaWasMarkedAsSpam', () => {
-            isOpen = false
-        })
-    "
-    x-data="{isOpen: false}"
-    x-show="isOpen"
-    @keydown.escape.window="isOpen = false"
-    @custom-show-mark-as-spam-idea-modal.window="
-        isOpen = true
-        $nextTick(()=>$refs.confirmButton.focus())
-    "
-    class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-    <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-    <div class="fixed inset-0 z-10 overflow-y-auto">
-        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <div
-                x-show="isOpen"
-                x-transition:enter.duration.200ms
-                x-transition:leave.duration.200ms
-                x-transition.scale.origin.top
-                class="modal relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                    <div class="sm:flex sm:items-start">
-                        <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                            <svg class="h-6 w-6 text-red" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
-                            </svg>
-                        </div>
-                        <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                            <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">Mark Idea as Spam</h3>
-                            <div class="mt-2">
-                                <p class="text-sm text-gray-500">Are you sure you want to mark this idea as spam? This action cannot be undone.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                    <button wire:click.prevent="markAsSpam" x-ref="confirmButton" type="button" class="inline-flex w-full justify-center rounded-md bg-blue px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-hover focus:bg-blue-hover sm:ml-3 sm:w-auto">Mark as Spam</button>
-                    <button @click.prevent="isOpen = false" type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<x-modal-confirm
+    :listenEvent="'ideaWasMarkedAsSpam'"
+    :customEventListener="'custom-show-mark-as-spam-idea-modal'"
+    :title="'Mark Idea as Spam'"
+    :description="'Are you sure you want to mark this idea as spam? This action cannot be undone.'"
+    :confirmEventName="'markAsSpam'"
+    :confirmButtonName="'Mark as Spam'"/>
